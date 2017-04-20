c968740bc85269ebec6271c248bbbe426b42e5ee
Matt MacNeil <matt@mattmacneil.com>
2017-04-20 16:38:44 -0400

    Added already complete files.

---
+++ index.php
@@ -0,0 +1,257 @@
+<?php
+    include "head.php";
+?>
+<div id="container" class="animate-bottom" style="display:none;">
+	<header>
+		<div id="header-inner">
+			<div>
+				<img src="images/logo.png" alt="Logo that I lifted from Pinterest." />
+			</div>
+			<div>
+				Matt MacNeil
+			</div>
+			<nav>
+				<a href="#work">work</a><a href="#about">about</a><a href="#contact">contact</a>
+			</nav><!-- nav -->
+			<div class="clearfix"></div>
+		</div><!-- header inner -->
+	</header><!-- header -->
+	<section id="introduction">
+		<h1><em>Matt MacNeil:</em> <span>Boston Web Designer & <br /></span> Developer Portfolio<em>.</em><span> for 2017.</span></h1>
+	</section><!-- introduction section -->
+	<section id="work">
+		<div class="work-cell work-cell-1">
+			<a href="http://www.bostoncrewrooms.com" target="_blank">
+			<div class="work-cell-overlay">
+				Boston Crew Room Booking
+			</div><!-- work cell overlay -->
+			</a>
+		</div><!-- work cell -->
+		<div class="work-cell work-cell-2">
+			<a href="http://www.kingsofcalabria.com/mglast" target="_blank">
+			<div class="work-cell-overlay">
+				Machine Gun Studios
+			</div><!-- work cell overlay -->
+			</a>
+		</div><!-- work cell -->
+		<div class="work-cell work-cell-3">
+			<a href="http://www.mattmacneil.com/work/marconi-development-group/" target="_blank">
+			<div class="work-cell-overlay">
+				Marconi Development
+			</div><!-- work cell overlay -->
+			</a>
+		</div><!-- work cell -->
+		<div class="work-cell work-cell-4">
+			<a href="http://www.littlefield1187.com/" target="_blank">
+			<div class="work-cell-overlay">
+				Littlefield #stylebythesea
+			</div><!-- work cell overlay -->
+			</a>
+		</div><!-- work cell -->
+		<div class="work-cell work-cell-5">
+			<a href="http://www.mattmacneil.com/work/the-fit-future" target="_blank">
+			<div class="work-cell-overlay">
+				The Fit Future by Standfast
+			</div><!-- work cell overlay -->
+			</a>
+		</div><!-- work cell -->
+		<div class="work-cell work-cell-6">
+			<a href="http://www.bethanytransportation.com/" target="_blank">
+			<div class="work-cell-overlay">
+				Bethany Transportation
+			</div><!-- work cell overlay -->
+			</a>
+		</div><!-- work cell -->
+		<div class="work-cell work-cell-7">
+			<a href="http://www.mattmacneil.com/work/temporada-na-praia/" target="_blank">
+			<div class="work-cell-overlay">
+				Temporada na Praia
+			</div><!-- work cell overlay -->
+			</a>
+		</div><!-- work cell -->
+		<div class="work-cell work-cell-8">
+			<a href="http://www.standbyspot.com/" target="_blank">
+			<div class="work-cell-overlay">
+				The Standby Spot Directory
+			</div><!-- work cell overlay -->
+			</a>
+		</div><!-- work cell -->
+		<div class="work-cell work-cell-9">
+			<div id="myBtn" class="work-cell-overlay">
+				Agency Work & Experience
+			</div><!-- work cell overlay -->
+		</div><!-- work cell -->
+	</section><!-- work -->
+	<section id="about">
+		<div class="about-left">
+			<span>About</span>
+		</div><!-- about left -->
+		<div class="about-right">
+			<span>I am a frontend designer and developer. I'm based in Boston but I travel for contracts anywhere in North America. I've been at it for about eight years as a freelancer & contracting with agencies in the area.</span>
+			<span>I like clean lines, custom builds, unique layouts, and making pixel-perfect unique designs from scratch.</span>
+		</div><!-- about right -->
+		<div class="clearfix"></div>
+		<div class="about-left">
+			<span>Approach</span>
+		</div><!-- about left -->
+		<div class="about-right">
+			<span>I prefer to build without frameworks so I can hone in on the small stuff. Yet I do understand the role that they play in modern development, so I am proficient in Bootstrap and Foundation. For custom stuff I usually go back to the ie11 era, but I can go back to ie8 if needed.</span>
+			<span>I focus on front-end builds, but I can find my way around most backend technology. I'm always happy to learn something new.</span>
+		</div><!-- about right -->
+		<div class="clearfix"></div>
+		<div class="about-left about-left-toolkit">
+			<span>Toolkit</span>
+		</div><!-- about left -->
+		<div class="about-right">
+			<div id="toolkit-container">
+				<div id="toolkit-mobile-intro">
+					<em>Toolkit :</em>
+				</div><!-- toolkit mobile intro -->
+				<div id="toolkit-intro">
+					An incomplete list,<br />
+					but to give you an idea:
+				</div><!-- toolkit intro -->
+				<div class="toolkit-row toolkit-row-first">
+					<div class="toolkit-cell">
+						<div class="toolkit-image toolkit-image-1">
+						</div><!-- toolkit image -->
+						<em>Cs</em>
+						<em>CSS3</em>
+					</div><!-- toolkit cell -->
+					<div class="toolkit-cell">
+						<div class="toolkit-image toolkit-image-2">
+						</div><!-- toolkit image -->
+						<em>H</em>
+						<em>HTML5</em>
+					</div><!-- toolkit cell -->
+					<div class="clearfix"></div>
+				</div><!-- toolkit row -->
+				<div class="toolkit-row">
+					<div class="toolkit-cell">
+						<div class="toolkit-image toolkit-image-3">
+						</div><!-- toolkit image -->
+						<em>Js</em>
+						<em>Javascript</em>
+					</div><!-- toolkit cell -->
+					<div class="toolkit-cell">
+						<div class="toolkit-image toolkit-image-4">
+						</div><!-- toolkit image -->
+						<em>Jq</em>
+						<em>JQuery</em>
+					</div><!-- toolkit cell -->
+					<div class="toolkit-cell toolkit-cell-mobile-off">
+						<div class="toolkit-image toolkit-image-5">
+						</div><!-- toolkit image -->
+						<em>Bs</em>
+						<em>Bootstrap</em>
+					</div><!-- toolkit cell -->
+					<div class="toolkit-cell">
+						<div class="toolkit-image toolkit-image-6">
+						</div><!-- toolkit image -->
+						<em>Fd</em>
+						<em>Foundation</em>
+					</div><!-- toolkit cell -->
+					<div class="toolkit-cell">
+						<div class="toolkit-image toolkit-image-7">
+						</div><!-- toolkit image -->
+						<em>P</em>
+						<em>PHP</em>
+					</div><!-- toolkit cell -->
+					<div class="clearfix"></div>
+				</div><!-- toolkit row -->
+				<div class="toolkit-row">
+					<div class="toolkit-cell">
+						<div class="toolkit-image toolkit-image-8">
+						</div><!-- toolkit image -->
+						<em>G</em>
+						<em>Git</em>
+					</div><!-- toolkit cell -->
+					<div class="toolkit-cell">
+						<div class="toolkit-image toolkit-image-9">
+						</div><!-- toolkit image -->
+						<em>Wp</em>
+						<em>Wordpress</em>
+					</div><!-- toolkit cell -->
+					<div class="toolkit-cell">
+						<div class="toolkit-image toolkit-image-10">
+						</div><!-- toolkit image -->
+						<em>Gu</em>
+						<em>Gulp</em>
+					</div><!-- toolkit cell -->
+					<div class="toolkit-cell toolkit-cell-mobile-off">
+						<div class="toolkit-image toolkit-image-11">
+						</div><!-- toolkit image -->
+						<em>Gr</em>
+						<em>Grunt</em>
+					</div><!-- toolkit cell -->
+					<div class="toolkit-cell toolkit-sass">
+						<a href="css/sass/development.scss" target="_blank">
+						<div>
+							<div class="toolkit-image toolkit-image-12">
+							</div><!-- toolkit image -->
+							<em>Sa</em>
+							<em>SASS</em>
+						</div>
+						</a>
+						<div id="toolkit-outro-snipe">
+						</div><!-- toolkit outro snipe -->
+					</div><!-- toolkit cell -->
+					<div class="clearfix"></div>
+				</div><!-- toolkit row -->
+				<div class="toolkit-row">
+					<div class="toolkit-cell">
+						<div class="toolkit-image toolkit-image-13">
+						</div><!-- toolkit image -->
+						<em>Ps</em>
+						<em>Photoshop</em>
+					</div><!-- toolkit cell -->
+					<div class="toolkit-cell">
+						<div class="toolkit-image toolkit-image-14">
+						</div><!-- toolkit image -->
+						<em>Ai</em>
+						<em>Illustrator</em>
+					</div><!-- toolkit cell -->
+					<div class="clearfix"></div>
+				</div><!-- toolkit row -->
+				<div id="toolkit-outro">
+					<a href="css/sass/development.scss" target="_blank">View Site<br />SASS File</a>
+				</div><!-- toolkit outro -->
+			</div><!-- toolkit container -->
+		</div><!-- about right -->
+		<div class="clearfix"></div>
+	</section><!-- about section -->
+	<section id="contact">
+		<div id="contact-inner">
+			<div id="contact-image">
+				<div id="contact-image-overlay">
+					<div id="contact-image-color">
+					</div><!-- contact image color -->
+				</div><!-- contact image overlay -->
+			</div><!-- contact image -->
+			<div id="contact-side">
+				<h2>
+					<?php echo date("m.d.y", time() - 60 * 60 * 96); ?>:<br />
+					How do you do?
+				</h2>
+				<span>I'm looking for a new gig. Part-time, extended contract, internship, or anything else works for me. I have my work flow down pretty good so I'd like some quick PSD to HTML-type gigs (tight deadlines are ok :P). If you think I'd be a good fit for your team, contact me!</span>
+				<div id="contact-side-buttons">
+					<a href="mailto:matt@mattmacneil.com">
+					<div class="contact-side-button">
+						matt@mattmacneil.com
+					</div><!-- contact side button -->
+					</a>
+					<a href="tel:617-820-3472">
+					<div class="contact-side-button">
+						+1 617-444-9646
+					</div><!-- contact side button -->
+					</a>
+					<div class="clearfix"></div>
+				</div><!-- contact side buttons -->
+			</div><!-- contact side -->
+			<div class="clearfix"></div>
+		</div><!-- contact inner -->
+	</section><!-- contact section -->
+</div><!-- container -->
+<?php 
+    include "footer.php";
+?>
